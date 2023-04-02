c6a793e9a44e28b9c576f28c30728d627d0ff677
shawnbirring <shawnbirring21@gmail.com>
2023-04-02 00:26:28 -0700

    fixed query

--- question4.sql
+++ question4.sql
@@ -5,4 +5,5 @@
 SELECT COUNT(*) AS breaker_shifts_this_week
 FROM Shift
 WHERE Shift_Is_Breaker = 1
-AND Sch_ID = 'SCH0000001';
+AND Shift_Start_Time >= DATEADD(wk, DATEDIFF(wk, 0, GETDATE()), 0)
+AND Shift_Start_Time < DATEADD(wk, DATEDIFF(wk, 0, GETDATE()) + 1, 0);
