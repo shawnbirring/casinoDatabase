eb061140ded5c8d669e653c4a847b03ba027a202
shawnbirring <shawnbirring21@gmail.com>
2023-04-02 00:30:46 -0700

    commit

--- question4.sql
+++ question4.sql
@@ -2,7 +2,7 @@
  * How many breaker shifts are scheduled this week?
  */
 
-SELECT COUNT(*) AS breaker_shifts_this_week
+SELECT COUNT(*) AS Breaker_Shifts_This_Week
 FROM Shift
 WHERE Shift_Is_Breaker = 1
 AND Shift_Start_Time >= DATEADD(wk, DATEDIFF(wk, 0, GETDATE()), 0)
