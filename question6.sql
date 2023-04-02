19c3494455de9a82c54ae4dc6d0e6707e2a7196c
shawnbirring <shawnbirring21@gmail.com>
2023-04-01 22:32:03 -0700

    fixed query

--- question6.sql
+++ question6.sql
@@ -8,6 +8,6 @@ WHERE Employee.Emp_Role = 'R04'
 AND Employee.Emp_Num NOT IN (
     SELECT Shift_Assigned_To
     FROM Shift
-    WHERE Section_ID = 'SEC01'
+    WHERE Section_ID = 'SEC0000001'
     AND Shift_Start_Time >= DATEADD(month, -1, GETDATE())
 );
