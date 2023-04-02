c6b7f4f0cc1624e7999ea58146894be45a1e213a
shawnbirring <shawnbirring21@gmail.com>
2023-04-02 00:32:03 -0700

    commit

--- question8.sql
+++ question8.sql
@@ -2,7 +2,7 @@
  * List of active WW sorted by date and employee
  */
 
-SELECT WW_ID, WW_Recipient, WW_Issuer, WW_Details, WW_Date_Issued
+SELECT *
 FROM Written_Warning
 WHERE WW_Is_Active = 'Yes'
 ORDER BY WW_Date_Issued, WW_Recipient;
