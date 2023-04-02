7a4c45e6e52d5fd11fa904657f8d87c9e16e575b
briankw0n <113305772+briankw0n@users.noreply.github.com>
2023-04-01 22:21:30 -0700

    Update question1.sql

--- question1.sql
+++ question1.sql
@@ -1,3 +1,13 @@
 /*
  * Number of hours each employee worked per week?
  */
+
+USE Casino_DB;
+GO
+
+SELECT e.Emp_Num, SUM(DATEDIFF(hour, sh.Shift_Start_Time, sh.Shift_End_Time)) as hours_worked_per_week
+FROM Employee e
+JOIN Shift sh ON e.Emp_Num = sh.Shift_Assigned_To
+JOIN Schedule s ON sh.Sch_ID = s.Sch_ID
+GROUP BY e.Emp_Num, s.Sch_Start_Date
+ORDER BY e.Emp_Num;
