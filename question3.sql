a7ccc7fbed2dc4aed18ced66c500f74366dde761
Hyuk Park <hyukpk@hotmail.com>
2023-04-02 21:22:23 -0700

    fixed question 3 and 15

--- question3.sql
+++ question3.sql
@@ -11,6 +11,6 @@ AND Employee.Emp_Num NOT IN (
     SELECT Shift_Assigned_To
     FROM Shift
     WHERE Shift_Is_Breaker = '0'
-    AND Shift_Covered_By = NULL;
+    AND Shift_Covered_By = NULL
     AND Shift_Start_Time >= DATEADD(month, -1, GETDATE())
 );
